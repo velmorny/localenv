c8405d505ca2cd9a31b3605e81e9b5d50a42929a
inoy <inoy@velmorn.dev>
2022-03-27 18:05:41 +0900

    Add go-task

--- init.sh
+++ init.sh
@@ -3,11 +3,15 @@
 BASE_DIR="$(pwd -P)"
 
 OS="`uname`"
-SETUP_SCRIPT_PATH="${BASE_DIR}/scripts/init_mac.sh"
+SETUP_SCRIPT_PATH="${BASE_DIR}/scripts/init_darwin.sh"
 
 if [[ "$OS" == "Linux" ]]; then
     OS='Linux'
     SETUP_SCRIPT_PATH="${BASE_DIR}/scripts/init_linux.sh"
 fi
 
+echo "[localenv] setting up with init script"
 /bin/bash $SETUP_SCRIPT_PATH
+
+echo "[localenv] setting up with go-task"
+task apply
--- scripts/init_darwin.sh
+++ scripts/init_darwin.sh
@@ -2,20 +2,18 @@
 
 set -e -u
 
-echo "[Mac Setup] Start Mac setup scripts"
-
 # Installing Homebrew
 if ! command -v brew > /dev/null 2>&1; then
-  echo "[Mac Setup] Installing brew"
+  echo "[localenv] Installing brew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-  echo "[Mac Setup] brew is already installed -> skipped"
+  echo "[localenv] brew is already installed -> skipped"
 fi
 
 # Installing go-task
 if ! brew list --formula go-task > /dev/null 2>&1; then
-  echo "[Mac Setup] Installing go-task"
+  echo "[localenv] Installing go-task"
   brew install go-task/tap/go-task
 else
-  echo "[Mac Setup] go-task is already installed with brew -> skipped"
+  echo "[localenv] go-task is already installed with brew -> skipped"
 fi
