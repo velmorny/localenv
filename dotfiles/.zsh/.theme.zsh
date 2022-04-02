20e338f0e57de8c7ce8721dcfdbcb89175a81a23
inoy <inoy@velmorn.dev>
2022-04-02 15:56:07 +0900

    change zsh prompt

--- dotfiles/.zsh/.theme.zsh
+++ dotfiles/.zsh/.theme.zsh
@@ -1,1 +1,10 @@
-PROMPT=$'\n> '
+# enable to use variables in prompt
+setopt PROMPT_SUBST
+
+# define local color
+local_red="%F{168}"
+local_green="%F{113}"
+local_reset="%f"
+
+# set prompt
+PROMPT=$'\n%{$local_green%}%~%{$local_reset%}\n%(?.%{%F{white}%}.%{$local_red%})%(!.#.>) '
