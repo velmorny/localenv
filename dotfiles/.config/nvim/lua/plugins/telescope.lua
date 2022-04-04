fdf73752c8bcc1201657ae3cba6640846ee3a7eb
inoy <inoy@velmorn.dev>
2022-04-04 23:23:53 +0900

    change key mapping

--- dotfiles/.config/nvim/lua/plugins/telescope.lua
+++ dotfiles/.config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@
 
 -- key mappings
 vim.api.nvim_set_keymap('n', '[telescope]', '', { noremap = true })
-vim.api.nvim_set_keymap('n', ' t', '[telescope]', {})
+vim.api.nvim_set_keymap('n', ' f', '[telescope]', {})
 vim.api.nvim_set_keymap('n', '[telescope]f', ':<C-u>Telescope find_files hidden=true theme=get_dropdown<CR>', {})
 vim.api.nvim_set_keymap('n', '[telescope]l', ':<C-u>Telescope live_grep theme=get_dropdown<CR>', {})
 vim.api.nvim_set_keymap('n', '[telescope]b', ':<C-u>Telescope buffers theme=get_dropdown<CR>', {})
