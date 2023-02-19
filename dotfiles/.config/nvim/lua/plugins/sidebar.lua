46b2e6d7a455223777034c5c7ed4ccf66a8a146c
inoy <inoy@velmorn.dev>
2023-02-19 19:16:53 +0900

    Add sidebar

---
+++ dotfiles/.config/nvim/lua/plugins/sidebar.lua
@@ -0,0 +1,26 @@
+vim.api.nvim_set_keymap('n', '[sidebar]', '', { noremap = true })
+vim.api.nvim_set_keymap('n', ' s', '[sidebar]', {})
+vim.api.nvim_set_keymap('n', '[sidebar]s', ':<C-u>SidebarNvimToggle<CR>', {})
+
+require("sidebar-nvim").setup({
+    disable_default_keybindings = 0,
+    bindings = nil,
+    open = true,
+    side = "left",
+    initial_width = 30,
+    hide_statusline = false,
+    update_interval = 1000,
+    sections = { "git", "diagnostics", "buffers", "files", "symbols" },
+    section_separator = {"", "-----", ""},
+    section_title_separator = {""},
+    containers = {
+        attach_shell = "/bin/sh", show_all = true, interval = 5000,
+    },
+    datetime = { format = "%a %b %d, %H:%M", clocks = { { name = "local" } } },
+    git = { icon = ">" },
+    diagnostics = { icon = ">" },
+    buffers = { icon = ">" },
+    files = { icon = ">" },
+    symbols = { icon = ">" },
+    todos = { ignored_paths = { "~" } },
+})
